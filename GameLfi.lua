181c378907c253b5ca4e90af4da9e97cd71397c5
SaiyKub <tirphphwaengwrrn@gmail.com>
2025-01-06 21:28:56 +0700

    GameLfi.lua

--- GameLfi.lua
+++ GameLfi.lua
@@ -2,6 +2,6 @@ getgenv().Games = {
   [2753915549] = "https://raw.githubusercontent.com/SaiyKub/MavixHub/refs/heads/main/MavixHub_BloxFruit.txt", -- Blox Fruit Sea 1
   [4442272183] = "https://raw.githubusercontent.com/SaiyKub/MavixHub/refs/heads/main/MavixHub_BloxFruit.txt", -- Blox Fruit Sea 2
   [7449423635] = "https://raw.githubusercontent.com/SaiyKub/MavixHub/refs/heads/main/MavixHub_BloxFruit.txt", -- Blox Fruit Sea 3
-  [16732694052] = "https://raw.githubusercontent.com/SaiyKub/MavixHub/refs/heads/main/MavixHub_Fisch.txt?token=GHSAT0AAAAAAC4MFIBQR3DYOIJOLH74WUIOZ33ZYDA", -- Fisch
+  [16732694052] = "https://raw.githubusercontent.com/SaiyKub/MavixHub/refs/heads/main/MavixHub_Fisch.txt", -- Fisch
 }
 
